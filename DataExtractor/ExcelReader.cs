36f1ad5ffe06b4c1429ef5212b0b4789b934a27e
Jordan Mryyan <jmryyan@ksu.edu>
2018-03-08 23:17:04 -0600

    Fixed issue that broke the excel reader when a cell contained mulitple lines.
    This solution still is not perfect but the main issue has been fixed.
    Next commit I will escape " from the string so I don't have issues with
    cells containing the " character. This solution is MUCH slower than the
    original but it keeps the integrity of the data.

--- DataExtractor/ExcelReader.cs
+++ DataExtractor/ExcelReader.cs
@@ -16,18 +16,38 @@ namespace DataExtractor
         {
             Application excel = new Application();
             Workbook workBook = excel.Workbooks.Open(filePath);
-            workBook.SaveAs(filePath + ".csv", XlFileFormat.xlCSVWindows);
-            workBook.Close(true);
-            excel.Quit();
+            Worksheet sheet = workBook.Sheets[1];
+            Range range = sheet.UsedRange;
 
-            List<string> valueList = null;
-            using (StreamReader sr = new StreamReader(filePath + ".csv"))
+            int rows = range.Rows.Count;
+            int cols = range.Columns.Count;
+
+            List<string> valueList = new List<string>();
+            StringBuilder line = new StringBuilder();
+
+            for (int i = 1; i <= rows; i++)
             {
-                string content = sr.ReadToEnd();
-                valueList = new List<string>(content.Split(new string[] { "\r\n" }, StringSplitOptions.None));
+                for (int j = 1; j <= cols; j++)
+                {
+                    var cellObject = range.Cells[i, j];
+                    var cellValue = range.Cells[i, j].Value2;
+
+                    if (cellObject != null)
+                    {
+                        string contentFormat = "\"{0}\"";
+                        string content = (cellValue == null) ? "" : cellValue.ToString();
+                        line.Append(String.Format(contentFormat, content));
+                        line.Append(',');
+                    }
+                }
+                line.Length--; // Remove the last comma
+                valueList.Add(line.ToString());
+                Console.WriteLine(line.ToString());
+                line.Clear();
             }
 
-            new FileInfo(filePath + ".csv").Delete();
+            workBook.Close(true);
+            excel.Quit();
 
            return valueList;
         }
